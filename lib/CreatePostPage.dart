a0f46dbbbdb06706a7a2bdc5beac04cceab26e26
Litian Zhang <litian_zhang17@163.com>
2021-01-23 16:17:45 -1000

    Add images picker feature

--- lib/CreatePostPage.dart
+++ lib/CreatePostPage.dart
@@ -1,6 +1,8 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 import 'package:qanda/Post.dart';
 import 'package:qanda/UniversalValues.dart';
 
@@ -16,6 +18,61 @@ class _CreatePostPageState extends State<CreatePostPage>{
 
   var title = "";
   var content = "";
+
+  List<Asset> images = List<Asset>();
+  String _error = 'No Error Detected';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget buildGridView() {
+    return GridView.count(
+      crossAxisCount: 3,
+      children: List.generate(images.length, (index) {
+        Asset asset = images[index];
+        return AssetThumb(
+          asset: asset,
+          width: 300,
+          height: 300,
+        );
+      }),
+    );
+  }
+
+  Future<void> loadAssets() async {
+    List<Asset> resultList = List<Asset>();
+    String error = 'No Error Detected';
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 300,
+        enableCamera: true,
+        selectedAssets: images,
+        cupertinoOptions: CupertinoOptions(takePhotoIcon: "chat"),
+        materialOptions: MaterialOptions(
+          actionBarColor: "#abcdef",
+          actionBarTitle: "Example App",
+          allViewTitle: "All Photos",
+          useDetailsView: false,
+          selectCircleStrokeColor: "#000000",
+        ),
+      );
+    } on Exception catch (e) {
+      error = e.toString();
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      images = resultList;
+      _error = error;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,6 +105,28 @@ class _CreatePostPageState extends State<CreatePostPage>{
                         ),
                       ),
                     ),
+                  ),
+
+
+
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    child:
+                    Column(
+                      children: <Widget>[
+                        Center(child: Text('Error: $_error')),
+                        RaisedButton(
+                          child: Text("Pick images"),
+                          onPressed: loadAssets,
+                        ),
+
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 200,
+                    child: buildGridView(),
                   ),
 
                   Container(
