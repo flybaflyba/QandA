c5dd1ea637e9c00cc804300f978a4f53c6d7cea7
Litian Zhang <litian_zhang17@163.com>
2021-01-25 17:29:10 -1000

    Imporve Create Post Page UI

--- lib/CreatePostPage.dart
+++ lib/CreatePostPage.dart
@@ -11,6 +11,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_absolute_path/flutter_absolute_path.dart';
 import 'package:multi_image_picker/multi_image_picker.dart';
+import 'package:nice_button/nice_button.dart';
 import 'package:qanda/Post.dart';
 import 'package:qanda/UniversalFunctions.dart';
 import 'package:qanda/UniversalValues.dart';
@@ -178,122 +179,110 @@ class _CreatePostPageState extends State<CreatePostPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: ListView(
-            children: [
-              Container(
-                constraints: BoxConstraints(minWidth: 150, maxWidth: 800),
-                child: Column(
+      body:
+      Center(
+        child: Container(
+            constraints: BoxConstraints(minWidth: 150, maxWidth: 800),
+            child: ListView(
+              children: [
+                SizedBox(height: 20,),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child:
+                  TextField(
+                    style: TextStyle(fontSize: 25),
+                    textAlign: TextAlign.center,
+                    onChanged: (value){
+                      title = value;
+                    },
+                    decoration: InputDecoration(
+                      hintText: "Title",
+                      alignLabelWithHint: true,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                    ),
+                  ),
+                ),
+
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child:
+                  TextField(
+                    minLines: 1,
+                    maxLines: 100,
+                    textAlign: TextAlign.left,
+                    onChanged: (value){
+                      content = value;
+                    },
+                    decoration: InputDecoration(
+                      hintText: "Content",
+                      alignLabelWithHint: true,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // Container(
+                //   margin: EdgeInsets.all(20),
+                //   child:
+                //   Column(
+                //     children: <Widget>[
+                //       Center(child: Text('Error: $_error')),
+                //       RaisedButton(
+                //         child: Text("Pick images"),
+                //         onPressed: loadAssets,
+                //       ),
+                //
+                //     ],
+                //   ),
+                // ),
+
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: buildGridView(),
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    SizedBox(height: 20,),
-
-                    Container(
-                      margin: EdgeInsets.all(20),
-                      child:
-                      TextField(
-                        style: TextStyle(fontSize: 25),
-                        textAlign: TextAlign.center,
-                        onChanged: (value){
-                          title = value;
-                        },
-                        decoration: InputDecoration(
-                          hintText: "Title",
-                          alignLabelWithHint: true,
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blue, width: 1.0),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    Container(
-                      margin: EdgeInsets.all(20),
-                      child:
-                      TextField(
-                        minLines: 1,
-                        maxLines: 100,
-                        textAlign: TextAlign.left,
-                        onChanged: (value){
-                          content = value;
-                        },
-                        decoration: InputDecoration(
-                          hintText: "Content",
-                          alignLabelWithHint: true,
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blue, width: 1.0),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    // Container(
-                    //   margin: EdgeInsets.all(20),
-                    //   child:
-                    //   Column(
-                    //     children: <Widget>[
-                    //       Center(child: Text('Error: $_error')),
-                    //       RaisedButton(
-                    //         child: Text("Pick images"),
-                    //         onPressed: loadAssets,
-                    //       ),
-                    //
-                    //     ],
-                    //   ),
-                    // ),
-
-                    Container(
-                      margin: EdgeInsets.all(20),
-                      child: buildGridView(),
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 50),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(30),
-                        child: Container(
-                          height: 60,
-                          width: 120,
-                          child: RaisedButton(
-                            onPressed: () {
-
-                              // print(images);
-                              if (title != "" && content != "") {
-                                Post post = new Post(
-                                  title: title,
-                                  content: content,
-                                  author: FirebaseAuth.instance.currentUser.email,
-                                  createdTime: DateTime.now().toString(),
-                                  imageUint8Lists: imageUint8Lists,
-                                );
-                                post.printOut();
-                                post.create();
-                              } else {
-                                UniversalFunctions.showToast("Please complete both title and content.", UniversalValues.toastMessageTypeWarningColor);
-                              }
-                            },
-                            color: UniversalValues.buttonColor,
-                            child: Text(
-                              'Create',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20),
-                            ),
-                          ),
-                        ),
-                      ),
+                    NiceButton(
+                      width: 255,
+                      elevation: 8.0,
+                      radius: 52.0,
+                      text: "Post",
+                      background: UniversalValues.buttonColor,
+                      onPressed: () {
+                        // print(images);
+                        if (title != "" && content != "") {
+                          Post post = new Post(
+                            title: title,
+                            content: content,
+                            author: FirebaseAuth.instance.currentUser.email,
+                            createdTime: DateTime.now().toString(),
+                            imageUint8Lists: imageUint8Lists,
+                          );
+                          post.printOut();
+                          post.create();
+                        } else {
+                          UniversalFunctions.showToast("Please complete both title and content.", UniversalValues.toastMessageTypeWarningColor);
+                        }
+                      },
                     ),
                   ],
-                ),
-              ),
-            ],
-          )
+                )
+              ],
+            )
+        ),
       ),
+
     );
   }}
