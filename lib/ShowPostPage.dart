531917f643e26464efbe0a4a37ff3e787d274441
Litian Zhang <litian_zhang17@163.com>
2021-01-28 22:25:29 -1000

    Fix image might not load from network error, imporve ui

--- lib/ShowPostPage.dart
+++ lib/ShowPostPage.dart
@@ -19,7 +19,8 @@ class ShowPostPage extends StatefulWidget{
 
 class _ShowPostPageState extends State<ShowPostPage>{
 
-  int _current = 0;
+  int currentImageIndex = 0;
+  double imageSize = 0.3;
 
   @override
   Widget build(BuildContext context) {
@@ -37,88 +38,132 @@ class _ShowPostPageState extends State<ShowPostPage>{
 
                   if(snapshot.hasData){
                     final content = snapshot.data;
-                    print(snapshot.data.data());
+                    // print(snapshot.data.data());
                     Post post = new Post();
                     post.setPostWithDocumentSnapshot(snapshot.data);
 
                     var imgList = post.imageUrls;
 
                     final List<Widget> imageSliders = imgList.map((item) => Container(
+                      height:  MediaQuery.of(context).size.height,
                       child: Container(
                         margin: EdgeInsets.all(5.0),
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                            child: Stack(
-                              children: <Widget>[
-                                Image.network(item, fit: BoxFit.cover, width: 1000.0),
-                                // Positioned(
-                                //   bottom: 0.0,
-                                //   left: 0.0,
-                                //   right: 0.0,
-                                //   child: Container(
-                                //     decoration: BoxDecoration(
-                                //       gradient: LinearGradient(
-                                //         colors: [
-                                //           Color.fromARGB(200, 0, 0, 0),
-                                //           Color.fromARGB(0, 0, 0, 0)
-                                //         ],
-                                //         begin: Alignment.bottomCenter,
-                                //         end: Alignment.topCenter,
-                                //       ),
-                                //     ),
-                                //     padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                                //     child: Text(
-                                //       '${imgList.indexOf(item)}',
-                                //       style: TextStyle(
-                                //         color: Colors.white,
-                                //         fontSize: 20.0,
-                                //         fontWeight: FontWeight.bold,
-                                //       ),
-                                //     ),
-                                //   ),
-                                // ),
-                              ],
-                            )
+                        child: InkWell(
+                          onTap: () {
+                            print("tapped an image");
+                            // setState(() {
+                            //   if (imageSize == 0.3) {
+                            //     imageSize = 0.6;
+                            //   } else {
+                            //     imageSize = 0.3;
+                            //   }
+                            // });
+                          },
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                              child: Stack(
+                                children: <Widget>[
+                                  // if the image quality is too high, might not load because of time out error, we can also set scale to 0.1 for example
+                                  Center(child: Image.network(item, filterQuality: FilterQuality.low, fit: BoxFit.cover, width: MediaQuery.of(context).size.width * 0.9),)
+                                  // indicator on image
+                                  // Positioned(
+                                  //   bottom: 0.0,
+                                  //   left: 0.0,
+                                  //   right: 0.0,
+                                  //   child: Container(
+                                  //     decoration: BoxDecoration(
+                                  //       gradient: LinearGradient(
+                                  //         colors: [
+                                  //           Color.fromARGB(200, 0, 0, 0),
+                                  //           Color.fromARGB(0, 0, 0, 0)
+                                  //         ],
+                                  //         begin: Alignment.bottomCenter,
+                                  //         end: Alignment.topCenter,
+                                  //       ),
+                                  //     ),
+                                  //     padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                                  //     child: Text(
+                                  //       '${imgList.indexOf(item)}',
+                                  //       style: TextStyle(
+                                  //         color: Colors.white,
+                                  //         fontSize: 20.0,
+                                  //         fontWeight: FontWeight.bold,
+                                  //       ),
+                                  //     ),
+                                  //   ),
+                                  // ),
+                                ],
+                              )
+                          ),
                         ),
-                      ),
+                        ),
                     )).toList();
 
                     final contentToDisplay =
                     Column(
                       children: [
 
-                        UniversalWidgets.titleWidget(post.title),
+                       Padding(
+                           padding: EdgeInsets.only(bottom: 10),
+                           child:  Container(
+                             color: Colors.grey[300],
+                             child: Column(
+                               children: [
+                                 UniversalWidgets.titleWidget(post.title),
 
-                        Padding(
-                          padding: EdgeInsets.only(bottom: 20),
-                          child:
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Center(child: Text(post.topic + " " + post.course),),
-                              Center(child: Text(timeago.format(DateTime.fromMicrosecondsSinceEpoch(post.createdTime.microsecondsSinceEpoch)) + " by " + post.author),),
-                            ],
-                          ),
-                        ),
+                                 Padding(
+                                   padding: EdgeInsets.only(bottom: 20),
+                                   child:
+                                   Row(
+                                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                     children: [
+                                       Center(child: Text(post.topic + " " + post.course),),
+                                       Center(child: Text(timeago.format(DateTime.fromMicrosecondsSinceEpoch(post.createdTime.microsecondsSinceEpoch)) + " by " + post.author),),
+                                     ],
+                                   ),
+                                 ),
+                               ],
+                             ),
+                           ),
+                       ),
+
 
                         // Center(child: Text(DateFormat.yMEd().add_jms().format(DateTime.fromMicrosecondsSinceEpoch(post.createdTime.microsecondsSinceEpoch))),),
                         // Center(child: Text(timeago.format(DateTime.fromMicrosecondsSinceEpoch(post.createdTime.microsecondsSinceEpoch))),),
+
+
+                        // CarouselSlider(
+                        //   options: CarouselOptions(
+                        //     height: MediaQuery.of(context).size.height,
+                        //     viewportFraction: 1.0,
+                        //     enlargeCenterPage: false,
+                        //     // autoPlay: false,
+                        //   ),
+                        //   items: imgList.map((item) => Container(
+                        //     child: Center(
+                        //         child: Image.network(item, fit: BoxFit.cover, )
+                        //     ),
+                        //   )).toList(),
+                        // ),
+
 
                         Column(
                             children: [
                               CarouselSlider(
                                 items: imageSliders,
                                 options: CarouselOptions(
-                                    autoPlay: true,
+                                    height: MediaQuery.of(context).size.height * imageSize,
+                                    autoPlay: false,
                                     enlargeCenterPage: true,
-                                    aspectRatio: 2.0,
+                                    aspectRatio: 2,
                                     onPageChanged: (index, reason) {
                                       setState(() {
-                                        _current = index;
+                                        currentImageIndex = index;
                                       });
                                     }
                                 ),
                               ),
+                              // indicator
                               Row(
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: imgList.map((url) {
@@ -129,7 +174,7 @@ class _ShowPostPageState extends State<ShowPostPage>{
                                     margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
                                     decoration: BoxDecoration(
                                       shape: BoxShape.circle,
-                                      color: _current == index
+                                      color: currentImageIndex == index
                                           ? Color.fromRGBO(0, 0, 0, 0.9)
                                           : Color.fromRGBO(0, 0, 0, 0.4),
                                     ),
@@ -138,7 +183,8 @@ class _ShowPostPageState extends State<ShowPostPage>{
                               ),
                             ]
                         ),
-                        SizedBox(height: 20, child: Container(color: Colors.white,),),
+                        
+                        // SizedBox(height: 20, child: Container(color: Colors.white,),),
 
                         Container(
                           color: Colors.grey[300],
